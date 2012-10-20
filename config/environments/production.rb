32a57d9d482e2d3c8b848b57f18351450e9690be
saasbook <saasbook@saasbook.(none)>
2012-10-20 03:15:59 -0700

    Working stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ TaskManager::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
-  config.assets.precompile += ['static_pages.css']
+  config.assets.precompile += %w[ bootstrap*.css bootstrap.js ]
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
